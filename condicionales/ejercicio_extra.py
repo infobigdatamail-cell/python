a433e2669159f96d7c2fcb055245aff0cbb1f094
infobigdatamail-cell <info.bigdata.mail@gmail.com>
2026-03-02 23:06:59 +0100

    Create ejercicio_extra.py

---
+++ condicionales/ejercicio_extra.py
@@ -0,0 +1,15 @@
+# Pide al usuario que ingrese un número entero que represente la vidaActual del personaje.
+# Evalúa el valor ingresado:
+# Si vidaActual es menor o igual a 0, muestra por consola:
+#   "Game Over"
+# En caso contrario, muestra por consola:
+#   "Sigues en partida"
+
+# Se crea la variable local
+vidaActual = 0
+
+# Se usa la estructura condicional
+if vidaActual <= 0:
+    print ('Game Over')
+else:
+    print ('Sigues en partida')
